ac217ec36ed27cba79b1fcac7ad9648916206236
Mattia Giordano <mgiordan@e4r8p4.42roma.it>
2021-10-07 18:38:16 +0200

    added something to my_tree

--- Map.hpp
+++ Map.hpp
@@ -19,9 +19,9 @@
 # include "iterator.hpp"
 # include "MapIterator.hpp"
 
-/*# include <map>
+# include <map>
 
-std::map<int, int> t;*/
+std::map<int, int> t;
 
 namespace ft
 {
@@ -62,23 +62,19 @@ namespace ft
 
 		// construct/copy/destroy:
 		explicit map(const key_compare& comp = key_compare()) : _tree(key_compare(comp)) {};
-		/* TODO */
-		map(const key_compare& comp, const allocator_type& a) : _tree(key_compare(comp)/*,  allocator */) {} ;
+		map(const key_compare& comp, const allocator_type& a) : _tree(key_compare(comp), allocator_type(a)) {} ;
 		template <class InputIterator>
 			map(InputIterator first, InputIterator last, const key_compare& comp = key_compare())
 				: _tree(key_compare(comp))
 			{ insert(first, last); };
-		/* TODO */
 		template <class InputIterator>
 			map(InputIterator first, InputIterator last, const key_compare& comp, const allocator_type& a)
-				: _tree(key_compare(comp)/*,  allocator */)
+				: _tree(key_compare(comp), allocator_type(a))
 			{ insert(first, last); };
 		map(const map& m) : _tree(m._tree)
 			{ insert(m.begin(), m.end()); /* why do i have to insert em if i already copy-created mine? */ };
-		/* TODO */
-		explicit map(const allocator_type& a) : _tree(/* allocator */) {};
-		/* TODO */
-		map(const map& m, const allocator_type& a) : _tree(m._tree /* , allocator */)
+		explicit map(const allocator_type& a) : _tree(allocator_type(a)) {};
+		map(const map& m, const allocator_type& a) : _tree(m._tree, allocator_type(a))
 			{ insert(m.begin(), m.end()); /* why do i have to insert em if i already copy-created mine? */ };
 		~map();
 
@@ -87,8 +83,6 @@ namespace ft
 			if (this != &m)
 			{
                 _tree.clear();
-                _tree.value_comp() = m._tree.value_comp();
-                _tree.__copy_assign_alloc(m._tree); // needs implementation
                 insert(m.begin(), m.end());
             }
             return (*this);
@@ -149,13 +143,15 @@ namespace ft
 
 		// modifiers:
 		/* all these functs depend on tree implementation */
-		pair<iterator, bool> insert(const value_type& v); 
-		iterator insert(const_iterator position, const value_type& v);
+		pair<iterator, bool>	insert(const value_type& v)
+			{ return (_tree.insert(v)); };
+		iterator				insert(const_iterator position, const value_type& v)
+			{ return (_tree.insert(position, v)); };
 		template <class InputIterator>
 			void insert(InputIterator first, InputIterator last)
 			{
 				for (const_iterator e = cend(); first != last; ++first)
-                	insert(e, *first); // depends on iterator implementation
+                	insert(e, *first);
 			};
 
 		iterator  erase(const_iterator position);
@@ -164,27 +160,33 @@ namespace ft
 		void clear();
 
 		// observers:
-		allocator_type get_allocator() const { return (allocator_type(_tree.__alloc())); }; // needs check
-		key_compare    key_comp()      const { }; // TODO
-		value_compare  value_comp()    const { return (value_compare(key_comp())); };
+		allocator_type get_allocator() const
+			{ return (allocator_type(_tree._node_allocator())); }; // needs check
+		key_compare    key_comp()      const
+			{ return (key_compare(_tree._value_compare()))};
+		value_compare  value_comp()    const
+			{ return (value_compare(key_comp())); };
 
 		// map operations:
-		iterator find(const key_type& k);
-		const_iterator find(const key_type& k) const;
+		iterator		find(const key_type& k);
+		const_iterator	find(const key_type& k) const;
 
 		size_type      count(const key_type& k) const;
-		iterator lower_bound(const key_type& k);
-		const_iterator lower_bound(const key_type& k) const;
 
-		iterator upper_bound(const key_type& k);
-		const_iterator upper_bound(const key_type& k) const;
+		iterator		lower_bound(const key_type& k);
+		const_iterator	lower_bound(const key_type& k) const;
+
+		iterator		upper_bound(const key_type& k);
+		const_iterator	upper_bound(const key_type& k) const;
 
 		pair<iterator,iterator>             equal_range(const key_type& k);
 		pair<const_iterator,const_iterator> equal_range(const key_type& k) const;
 
-	private:
-		_tree_type _tree;
+		void swap(map& m)
+			{ _tree.swap(m._tree); };
 
+	private:
+		tree<value_type, key_compare, allocator_type>	_tree;
 	};
 
 	template <class Key, class T, class Compare, class Allocator>
--- my_tree.hpp
+++ my_tree.hpp
@@ -87,9 +87,10 @@ namespace ft
 	{
 		public:
 			typedef T 							node_value_type;
-			typedef tree_node<node_value_type>*	node_pointer;
+			typedef node_value_type*			value_pointer;
+ 			typedef tree_node<node_value_type>*	node_pointer;
 
-			node_value_type	value;
+			value_pointer	value;
 			node_pointer	right;
 			node_pointer	left;
 			node_pointer	parent;
@@ -99,17 +100,136 @@ namespace ft
 				{ return (parent); }
 			void set_parent(node_pointer p)
 				{ parent = p; }
-
+			tree_node() : value(nullptr), right(nullptr), left(nullptr), parent(nullptr), is_black(true) {};
 			tree_node(tree_node const & other)
 				{ *this = other; };
   			tree_node& operator=(tree_node const & other)
 			{
-				this->value = other.value;
+				this->value = other.value; // sistemare, questa non fa deep copy.
 				this->right = other.right;
 				this->left = other.left;
 				this->parent = other.parent;
 				this->is_black = other.is_black;
 				return (*this);
 			};
+	};
+
+	/*
+		tree
+	*/
+	template <class T, class Compare, class Allocator>
+	class tree
+	{
+		public:
+			typedef T											value_type;
+			typedef typename value_type::first_type				key_type;
+			typedef tree_node<T>								node_type;
+			typedef typename node_type::node_pointer			node_pointer;
+			typedef std::allocator<node_type>					node_allocator;
+			typedef Allocator									value_allocator;
+			typedef typename node_allocator::size_type			size_type;
+			typedef typename node_allocator::difference_type	difference_type;
+			typedef Compare										value_compare;
+
+			typedef tree_iterator<value_type, difference_type>			iterator;
+			typedef tree_const_iterator<value_type, difference_type>	const_iterator;
+			
+		private:
+			node_pointer							_begin_node;
+			node_pointer							_end_node;
+			pair<node_allocator, value_allocator>	_allocators;
+			size_type								_size;
+			value_compare							_comp;
+
+		public:
+			tree(value_allocator& alloc)
+				: _begin_node(nullptr), _end_node(nullptr), _allocators(make_pair(node_allocator(), alloc)), _size(0), _comp(value_compare()) {};
+			tree(value_compare& comp, value_allocator& alloc = value_allocator())
+				: _begin_node(nullptr), _end_node(nullptr), _allocators(make_pair(node_allocator(), alloc)), _size(0), _comp(comp) {};
+			// copy constructor just copies tree conf but not the elements
+			tree(tree const & other, value_allocator& alloc = value_allocator(other._value_allocator()))
+				: _begin_node(nullptr), _end_node(nullptr), _allocators(make_pair(node_allocator(node_allocator()), value_allocator(alloc))), _size(0), _comp(value_compare()) {};
+			~tree() { /*TODO */ };
+			tree& operator=(const tree& other)
+			{
+				if (this != &other)
+				{
+					_allocators.second = value_allocator(other._value_allocator());
+					_comp = value_compare(other._comp);					
+				}
+				return (*this);
+			}
+
+          	iterator begin()
+				{ return (iterator(_begin_node)); };
+			const_iterator begin() const
+				{ return (const_iterator(_begin_node)); }
+			iterator end()
+				{ return (iterator(_end_node)); };
+			const_iterator end() const
+				{ return (const_iterator(_end_node)); }
+			
+			node_allocator const &	_node_allocator() const
+				{ return (_allocators.first) };
+			value_allocator const & _value_allocator() const
+				{ return (_allocators.second) };
+			value_compare const & _value_compare() const
+				{ return (_comp) };
+
+			const size_type& size() const
+				{ return (_size); };
+			size_type max_size() const
+				{ return (_node_allocator().max_size()) };
+
+			pair<iterator, bool> insert(const value_type& v)
+			{
+				/* TODO */
+			}
+			iterator insert(const_iterator pos, const value_type& v)
+			{
+				/* TODO */
+			}
+			
+			iterator erase(const_iterator pos)
+			{
+				/* TODO */
+			}
+			iterator erase(const_iterator first, const_iterator last)
+			{
+				while (first != last)
+					first = erase(first);
+				return (iterator(last));
+			}
+			void erase(const key_type& k)
+			{
+				iterator i = find(k);
+				if (i == end())
+					return (0);
+				erase(i);
+				return (1);
+			}
+
+			void clear()
+			{
+				// add destroy method
+				_size = 0;
+				_begin_node = nullptr;
+				_end_node = nullptr; // maybe not necessary
+			}
+
+			void swap(tree& other)
+			{
+				ft::swap(this->_begin_node, other._begin_node);
+				ft::swap(this->_end_node, other._end_node);
+				this->_allocators.swap(other._allocators);
+				ft::swap(this->_size, other._size);
+				ft::swap(this->_comp, other._comp);
+			}
+			/* NEEDED FUNCTIONS 
+				- Find
+				- Count
+				- lower_bound/upper_bound
+				- Equal_range */
+
 	};
 };
